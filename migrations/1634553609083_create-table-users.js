9fd11c73e4291cfbb4ffe7f2465ac7be612045fa
Diki Rahman <dikirahman.me@gmail.com>
2021-10-18 19:35:47 +0700

    init

---
+++ migrations/1634553609083_create-table-users.js
@@ -0,0 +1,58 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+ 
+exports.up = (pgm) => {
+  pgm.createTable('users', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    role_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    profession: {
+      type: 'VARCHAR(50)',
+      notNull: false,
+    },
+    about: {
+      type: 'VARCHAR(50)',
+      notNull: false,
+    },
+    website: {
+      type: 'VARCHAR(50)',
+      notNull: false,
+    },
+    username: {
+      type: 'VARCHAR(50)',
+      unique: true,
+      notNull: true,
+    },
+    email: {
+      type: 'VARCHAR(50)',
+      unique: true,
+      notNull: true,
+    },
+    password: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    fullname: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    created_at: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    updated_at: {
+      type: 'TEXT',
+      notNull: true,
+    },
+  });
+};
+ 
+exports.down = (pgm) => {
+  pgm.dropTable('users');
+};
