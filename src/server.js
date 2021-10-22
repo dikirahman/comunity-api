0710e7d698db7509d8b4c930771f0be9bd539cce
Diki Rahman <dikirahman.me@gmail.com>
2021-10-22 08:42:11 +0700

    feat: creating server

--- src/server.js
+++ src/server.js
@@ -1,16 +1,40 @@
+require('dotenv').config();
+
 const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
+
+// users
+const users = require('./api/users');
+const UsersService = require('./services/postgres/UsersService');
+const UsersValidator = require('./validator/users');
 
 const init = async () => {
-    const server = Hapi.server({
-        port: 5000,
-        host: 'localhost',
-    });
- 
-    server.route(routes);
+  // user service
+  const usersService = new UsersService();
 
-    await server.start();
-    console.log(`Server berjalan pada ${server.info.uri}`);
-}
+  const server = Hapi.server({
+    port: process.env.PORT,
+    host: process.env.HOST,
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  // register internal plugin
+  await server.register([
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator
+      },
+    },
+  ]);
+
+  await server.start();
+
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
  
 init();
