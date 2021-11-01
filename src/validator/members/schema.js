28945313a8945efdb13c4754421e7dc90e6c4919
Diki Rahman <dikirahman.me@gmail.com>
2021-11-01 08:10:44 +0700

    refactor(role): change data type, string to number

--- src/validator/members/schema.js
+++ src/validator/members/schema.js
@@ -3,7 +3,7 @@ const Joi = require('joi');
 const MemberPayloadSchema = Joi.object({
   organizationId: Joi.string().required(),
   userId: Joi.string().required(),
-  role: Joi.string().required(),
+  role: Joi.number().required(),
 });
 
 module.exports = { MemberPayloadSchema };
