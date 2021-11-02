56ac77bf7daa1d08aad388eeb3e2f43aa341cd4a
Diki Rahman <dikirahman.me@gmail.com>
2021-11-02 07:57:21 +0700

    feat: adding organizations service

--- src/api/members/index.js
+++ src/api/members/index.js
@@ -4,8 +4,17 @@ const routes = require('./routes');
 module.exports = {
   name: 'members',
   version: '1.0.0',
-  register: async (server, { service, validator }) => {
-    const membersHandler = new MembersHandler(service, validator); 
+  register: async (server, { 
+    membersService,
+    organizationsService, 
+    validator 
+  }) => {
+    
+    const membersHandler = new MembersHandler(
+    membersService,
+    organizationsService,
+    validator
+    ); 
     server.route(routes(membersHandler));
   },
 };
